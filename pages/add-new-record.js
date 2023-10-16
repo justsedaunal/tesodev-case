2fa67a9e20a5588ccbbc194227f83cf8844bb796
Seda Unal <sedaylmz06@outlook.com>
2023-10-16 23:48:29 +0300

    add new record button css is done

---
+++ pages/add-new-record.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+export default function AddNewRecord() {
+  return (
+    <>
+      <Link href="/">
+        
+       {/* <button className='add-new-record-button' >Back to home</button>  */}
+      
+      </Link>
+      <button className='add-new-record-button' > Add new record </button>
+    </>
+  );
+}
