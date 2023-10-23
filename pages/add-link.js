071b42ee251b0cba8b0dbc2f6549a7257d6d727b
Seda Unal <sedaylmz06@outlook.com>
2023-10-23 23:51:23 +0300

    toast css is done

--- pages/add-link.js
+++ pages/add-link.js
@@ -34,77 +34,105 @@ const RecordForm = () => {
           </Link>
         </div>
       </div>
-      <div className="form-toast-container" >
-      <form onSubmit="{handleSubmit}" className="form">
-        <div>
-          <label htmlFor="name">Name Surname:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            // value={record.name}
-            // onChange={handleChange}
-            placeholder="Enter name and surname"
-            required
-          />
-        </div>
+      <div className="form-toast-container">
+        <form onSubmit="{handleSubmit}" className="form">
+          <div>
+            <label htmlFor="name">Name Surname:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              // value={record.name}
+              // onChange={handleChange}
+              placeholder="Enter name and surname"
+              required
+            />
+          </div>
 
-        <div>
-          <label htmlFor="country">Country:</label>
-          <input
-            type="text"
-            id="country"
-            name="country"
-            // value={record.country}
-            // onChange={handleChange}
-            placeholder="Enter name and surname"
-            required
-          />
-        </div>
+          <div>
+            <label htmlFor="country">Country:</label>
+            <input
+              type="text"
+              id="country"
+              name="country"
+              // value={record.country}
+              // onChange={handleChange}
+              placeholder="Enter name and surname"
+              required
+            />
+          </div>
 
-        <div>
-          <label htmlFor="city">City:</label>
-          <input
-            type="text"
-            id="city"
-            name="city"
-            // value={record.city}
-            // onChange={handleChange}
-            placeholder="Enter a country"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            // value={record.email}
-            // onChange={handleChange}
-            placeholder="Enter a websitel (https://xyz.com)"
-            required
-          />
-        </div>
+          <div>
+            <label htmlFor="city">City:</label>
+            <input
+              type="text"
+              id="city"
+              name="city"
+              // value={record.city}
+              // onChange={handleChange}
+              placeholder="Enter a country"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              // value={record.email}
+              // onChange={handleChange}
+              placeholder="Enter a websitel (https://xyz.com)"
+              required
+            />
+          </div>
 
-        <div>
-          <label htmlFor="website">Website:</label>
-          <input
-            type="url"
-            id="website"
-            name="website"
-            // value={record.website}
-            // onChange={handleChange}
-            placeholder="Enter name and surname"
-            required
-          />
-        </div>
+          <div>
+            <label htmlFor="website">Website:</label>
+            <input
+              type="url"
+              id="website"
+              name="website"
+              // value={record.website}
+              // onChange={handleChange}
+              placeholder="Enter name and surname"
+              required
+            />
+          </div>
 
-        <button type="submit" className="submit-button" >Add</button>
-      </form>
-      <div>error tost</div>
+          <button type="submit" className="submit-button">
+            Add
+          </button>
+        </form>
+        <div className="toast-container">
+          <div className="close">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M15 9L9 15M9 9L15 15M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+                stroke="#090A0A"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+          <div className="toast-texts">
+            <div>
+              <p>Error while adding link element</p>
+              <p>Name and surname should contain at least 2 words</p>
+            </div>{" "}
+            <span>Error</span>
+          </div>
+
+          <div></div>
+        </div>
       </div>
-
     </>
   );
 };
